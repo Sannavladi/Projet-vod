32399a574568624f5b2fa8ae3de8509b891f36cc
Anna Shtanko <125276706+Sannavladi@users.noreply.github.com>
2023-06-01 17:48:48 +0200

    avec les categories

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_front_category')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{slug}', name: 'app_category_show')]
+    public function showCategory($slug, CategoryRepository $categoryRepository): Response
+    {
+        
+        $category = $categoryRepository->findOneBy(['slug' => $slug]);
+
+        
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+}
